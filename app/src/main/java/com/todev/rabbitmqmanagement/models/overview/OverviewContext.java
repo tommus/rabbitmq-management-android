21f8b26723d9b0640b2f708693d777a23af54d56
Tomasz Dzieniak <tomasz.dzieniak@to-dev.com>
2016-07-08 00:37:25 +0200

    Remove mistakenly left old class comments.

--- app/src/main/java/com/todev/rabbitmqmanagement/models/overview/OverviewContext.java
+++ app/src/main/java/com/todev/rabbitmqmanagement/models/overview/OverviewContext.java
@@ -5,10 +5,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.todev.rabbitmqmanagement.models.Context;
 
-/**
- * @author Tomasz Dzieniak
- * @since 09.06.16.
- */
 @JsonIgnoreProperties(ignoreUnknown = true) @JsonInclude(JsonInclude.Include.NON_NULL)
 public class OverviewContext extends Context {
 
