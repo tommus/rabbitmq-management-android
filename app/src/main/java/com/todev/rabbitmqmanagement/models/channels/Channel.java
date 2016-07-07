c1d9b5aae1e91e1480cf4781f9289c826bd60cc3
Tomasz Dzieniak <tomasz.dzieniak@to-dev.com>
2016-07-08 01:46:49 +0200

    Remove redundant empty line after copyright notes in some files.

--- app/src/main/java/com/todev/rabbitmqmanagement/models/channels/Channel.java
+++ app/src/main/java/com/todev/rabbitmqmanagement/models/channels/Channel.java
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package com.todev.rabbitmqmanagement.models.channels;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
