8ff3c3f1c6b185924dcdacfe2daf6ae9cdc5e593
Tomasz Dzieniak <tomasz.dzieniak@to-dev.com>
2016-07-22 01:51:31 +0200

    Refactor queued messages fragment as custom view.

--- app/src/main/java/com/todev/rabbitmqmanagement/activities/OverviewActivity.java
+++ app/src/main/java/com/todev/rabbitmqmanagement/activities/OverviewActivity.java
@@ -17,6 +17,8 @@
  */
 package com.todev.rabbitmqmanagement.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -24,14 +26,35 @@ import android.view.View;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.todev.rabbitmqmanagement.R;
-import com.todev.rabbitmqmanagement.fragments.QueuedMessagesFragment;
+import com.todev.rabbitmqmanagement.models.overview.Overview;
+import com.todev.rabbitmqmanagement.service.RabbitMqService;
+import com.todev.rabbitmqmanagement.widget.QueuedMessagesIndicator;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class OverviewActivity extends AppCompatActivity {
 
   @BindView(R.id.activity_overview)
   View rootView;
 
-  QueuedMessagesFragment queuedMessagesFragment;
+  @BindView(R.id.queued_messages_indicator)
+  QueuedMessagesIndicator queuedMessagesIndicator;
+
+  private ScheduledExecutorService executorService;
+
+  private RabbitMqService rabbitMqService;
+
+  private TimerTask updateTimerTask = new TimerTask() {
+    @Override
+    public void run() {
+      rabbitMqService.getOverview().enqueue(new OverviewResponseCallback());
+    }
+  };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -40,16 +63,50 @@ public class OverviewActivity extends AppCompatActivity {
 
     ButterKnife.bind(this);
 
-    queuedMessagesFragment =
-        (QueuedMessagesFragment) getSupportFragmentManager().findFragmentById(R.id.queued_messages_fragment);
+    initializeRabbitMqService();
   }
 
   @Override
   protected void onResume() {
     super.onResume();
 
-    if (queuedMessagesFragment.getView() != null) {
-      animate(queuedMessagesFragment.getView(), rootView);
+    animate(queuedMessagesIndicator, rootView);
+    initializeScheduler();
+    startScheduler();
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+
+    stopScheduler();
+  }
+
+  private void initializeRabbitMqService() {
+    SharedPreferences sharedPreferences =
+        getApplicationContext().getSharedPreferences(getString(R.string.shared_preferences_key), Context.MODE_PRIVATE);
+
+    String url = sharedPreferences.getString(getString(R.string.shared_preferences_last_used_service_url), "");
+    int port = sharedPreferences.getInt(getString(R.string.shared_preferences_last_used_service_port), 15672);
+    String login = sharedPreferences.getString(getString(R.string.shared_preferences_last_used_login), "");
+    String password = sharedPreferences.getString(getString(R.string.shared_preferences_last_used_password), "");
+
+    rabbitMqService = RabbitMqService.Json.createService(url, port, login, password);
+  }
+
+  private void initializeScheduler() {
+    if (executorService == null || executorService.isShutdown()) {
+      executorService = Executors.newSingleThreadScheduledExecutor();
+    }
+  }
+
+  private void startScheduler() {
+    executorService.scheduleAtFixedRate(updateTimerTask, 0, 1, TimeUnit.SECONDS);
+  }
+
+  private void stopScheduler() {
+    if (!executorService.isShutdown()) {
+      executorService.shutdown();
     }
   }
 
@@ -58,5 +115,38 @@ public class OverviewActivity extends AppCompatActivity {
     view.setAlpha(0);
     view.setTranslationY(parent.getHeight());
     view.animate().alpha(1f).translationY(0).setDuration(600).setStartDelay(400);
+  }
+
+  private void updateQueuedMessagesIndicator(Response<Overview> response) {
+    Overview overview = response.body();
+
+    int ready = overview.getQueueTotals().getMessagesReady();
+    int unacked = overview.getQueueTotals().getMessagesUnacknowledged();
+    int total = overview.getQueueTotals().getMessages();
+
+    queuedMessagesIndicator.updateChart(ready, QueuedMessagesIndicator.SetIndex.READY);
+    queuedMessagesIndicator.updateChart(unacked, QueuedMessagesIndicator.SetIndex.UNACKED);
+    queuedMessagesIndicator.updateChart(total, QueuedMessagesIndicator.SetIndex.TOTAL);
+
+    queuedMessagesIndicator.updateReadyButton(getString(R.string.activity_overview_button_ready, ready));
+    queuedMessagesIndicator.updateUnackedButton(getString(R.string.activity_overview_button_unacked, unacked));
+    queuedMessagesIndicator.updateTotalButton(getString(R.string.activity_overview_button_total, total));
+  }
+
+  private class OverviewResponseCallback implements Callback<Overview> {
+
+    @Override
+    public void onResponse(Call<Overview> call, Response<Overview> response) {
+      if (!response.isSuccessful()) {
+        return;
+      }
+
+      updateQueuedMessagesIndicator(response);
+    }
+
+    @Override
+    public void onFailure(Call<Overview> call, Throwable t) {
+      // Do nothing on error. Just do not update the chart.
+    }
   }
 }
---
+++ widgets/src/main/java/com/todev/rabbitmqmanagement/widget/QueuedMessagesIndicator.java
@@ -0,0 +1,384 @@
+package com.todev.rabbitmqmanagement.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.DefaultAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.todev.rabbitmqmanagement.widgets.R;
+import java.util.concurrent.TimeUnit;
+
+public class QueuedMessagesIndicator extends FrameLayout {
+
+  protected String title;
+
+  protected int defaultColor;
+
+  protected int readyLineColor;
+
+  protected int readyButtonColor;
+
+  protected int unackedLineColor;
+
+  protected int unackedButtonColor;
+
+  protected int totalLineColor;
+
+  protected int totalButtonColor;
+
+  protected boolean valuesVisible;
+
+  protected String idleText;
+
+  protected VisibleRange visibleRange;
+
+  protected TextView titleTextView;
+
+  protected LineChart messagesChart;
+
+  protected Button readyButton;
+
+  protected Button unackedButton;
+
+  protected Button totalButton;
+
+  public QueuedMessagesIndicator(Context context, AttributeSet attrs) {
+    super(context, attrs);
+
+    inflate(context, R.layout.view_queued_messages_indicator, this);
+    initializeAttributes(context, attrs);
+    initializeWidgets();
+    initializeChart();
+    initializeCallbacks();
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+    invalidate();
+    requestLayout();
+  }
+
+  public int getReadyLineColor() {
+    return readyLineColor;
+  }
+
+  public void setReadyLineColor(int readyLineColor) {
+    this.readyLineColor = readyLineColor;
+    invalidate();
+    requestLayout();
+  }
+
+  public int getReadyButtonColor() {
+    return readyButtonColor;
+  }
+
+  public void setReadyButtonColor(int readyButtonColor) {
+    this.readyButtonColor = readyButtonColor;
+  }
+
+  public int getUnackedLineColor() {
+    return unackedLineColor;
+  }
+
+  public void setUnackedLineColor(int unackedLineColor) {
+    this.unackedLineColor = unackedLineColor;
+    invalidate();
+    requestLayout();
+  }
+
+  public int getUnackedButtonColor() {
+    return unackedButtonColor;
+  }
+
+  public void setUnackedButtonColor(int unackedButtonColor) {
+    this.unackedButtonColor = unackedButtonColor;
+    invalidate();
+    requestLayout();
+  }
+
+  public int getTotalLineColor() {
+    return totalLineColor;
+  }
+
+  public void setTotalLineColor(int totalLineColor) {
+    this.totalLineColor = totalLineColor;
+    invalidate();
+    requestLayout();
+  }
+
+  public int getTotalButtonColor() {
+    return totalButtonColor;
+  }
+
+  public void setTotalButtonColor(int totalButtonColor) {
+    this.totalButtonColor = totalButtonColor;
+    invalidate();
+    requestLayout();
+  }
+
+  public boolean isValuesVisible() {
+    return valuesVisible;
+  }
+
+  public void setValuesVisible(boolean valuesVisible) {
+    this.valuesVisible = valuesVisible;
+    invalidate();
+    requestLayout();
+  }
+
+  public String getIdleText() {
+    return idleText;
+  }
+
+  public void setIdleText(String idleText) {
+    this.idleText = idleText;
+    invalidate();
+    requestLayout();
+  }
+
+  public VisibleRange getVisibleRange() {
+    return visibleRange;
+  }
+
+  public void setVisibleRange(VisibleRange visibleRange) {
+    this.visibleRange = visibleRange;
+    invalidate();
+    requestLayout();
+  }
+
+  public void updateChart(int value, SetIndex index) {
+    LineData data = messagesChart.getData();
+    ILineDataSet set = data.getDataSetByIndex(index.getIndex());
+
+    if (set == null) {
+      set = createSet(getLineColor(index));
+      data.addDataSet(set);
+    }
+
+    data.addEntry(new Entry(set.getEntryCount(), value), index.getIndex());
+    data.notifyDataChanged();
+
+    messagesChart.notifyDataSetChanged();
+    messagesChart.setVisibleXRangeMaximum(getVisibleRange().getRange());
+    messagesChart.moveViewToX(data.getEntryCount());
+  }
+
+  public void updateReadyButton(String text) {
+    readyButton.setText(text);
+  }
+
+  public void updateUnackedButton(String text) {
+    unackedButton.setText(text);
+  }
+
+  public void updateTotalButton(String text) {
+    totalButton.setText(text);
+  }
+
+  protected void initializeAttributes(Context context, AttributeSet attrs) {
+    TypedArray array = context.getTheme().obtainStyledAttributes(attrs, R.styleable.QueuedMessagesIndicator, 0, 0);
+
+    try {
+      title = array.getString(R.styleable.QueuedMessagesIndicator_title);
+
+      defaultColor = ContextCompat.getColor(context, android.R.color.black);
+
+      readyLineColor = array.getColor(R.styleable.QueuedMessagesIndicator_readyLineColor,
+          ContextCompat.getColor(context, R.color.queued_messages_indicator_default_ready_color));
+
+      readyButtonColor = array.getColor(R.styleable.QueuedMessagesIndicator_readyButtonColor,
+          ContextCompat.getColor(context, R.color.queued_messages_indicator_default_ready_color));
+
+      unackedLineColor = array.getColor(R.styleable.QueuedMessagesIndicator_unackedLineColor,
+          ContextCompat.getColor(context, R.color.queued_messages_indicator_default_unacked_color));
+
+      unackedButtonColor = array.getColor(R.styleable.QueuedMessagesIndicator_unackedButtonColor,
+          ContextCompat.getColor(context, R.color.queued_messages_indicator_default_unacked_color));
+
+      totalLineColor = array.getColor(R.styleable.QueuedMessagesIndicator_totalLineColor,
+          ContextCompat.getColor(context, R.color.queued_messages_indicator_default_total_color));
+
+      totalButtonColor = array.getColor(R.styleable.QueuedMessagesIndicator_totalButtonColor,
+          ContextCompat.getColor(context, R.color.queued_messages_indicator_default_total_color));
+
+      valuesVisible = array.getBoolean(R.styleable.QueuedMessagesIndicator_valuesVisible,
+          getResources().getBoolean(R.bool.queued_messages_indicator_default_values_visible));
+
+      idleText = array.getString(R.styleable.QueuedMessagesIndicator_idleText);
+
+      int range = array.getInteger(R.styleable.QueuedMessagesIndicator_visibleRange, 0);
+
+      visibleRange = VisibleRange.fromRange(range);
+    } finally {
+      array.recycle();
+    }
+  }
+
+  protected void initializeWidgets() {
+    titleTextView = (TextView) findViewById(R.id.title);
+    messagesChart = (LineChart) findViewById(R.id.messages_chart);
+    readyButton = (Button) findViewById(R.id.ready_button);
+    unackedButton = (Button) findViewById(R.id.unacked_button);
+    totalButton = (Button) findViewById(R.id.total_button);
+
+    titleTextView.setText(title);
+    readyButton.setTextColor(getReadyButtonColor());
+    unackedButton.setTextColor(getUnackedButtonColor());
+    totalButton.setTextColor(getTotalButtonColor());
+  }
+
+  protected void initializeCallbacks() {
+    readyButton.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        toggleSetVisibility(SetIndex.READY);
+      }
+    });
+
+    unackedButton.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        toggleSetVisibility(SetIndex.UNACKED);
+      }
+    });
+
+    totalButton.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        toggleSetVisibility(SetIndex.TOTAL);
+      }
+    });
+  }
+
+  protected void initializeChart() {
+    messagesChart.getLegend().setPosition(Legend.LegendPosition.ABOVE_CHART_CENTER);
+    messagesChart.setDescription("");
+    messagesChart.setNoDataTextDescription(getIdleText());
+    messagesChart.setTouchEnabled(false);
+    messagesChart.setDragEnabled(false);
+    messagesChart.setPinchZoom(false);
+    messagesChart.getLegend().setEnabled(false);
+    messagesChart.getAxisLeft().setAxisMinValue(0);
+    messagesChart.getAxisLeft().setGranularity(1f);
+    messagesChart.getAxisRight().setEnabled(false);
+    messagesChart.getXAxis().setAxisMinValue(0);
+    messagesChart.getXAxis().setGranularity(1f);
+    messagesChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+    messagesChart.getXAxis().setValueFormatter(new DefaultAxisValueFormatter(0));
+
+    LineData data = new LineData();
+    data.setValueTextColor(Color.WHITE);
+    messagesChart.setData(data);
+  }
+
+  private LineDataSet createSet(int color) {
+    LineDataSet set = new LineDataSet(null, "");
+    set.setColor(color);
+    set.setCircleColor(color);
+    set.setCircleRadius(1f);
+    set.setFillColor(color);
+    set.setDrawValues(isValuesVisible());
+    return set;
+  }
+
+  private int getLineColor(SetIndex index) {
+    switch (index) {
+      case READY:
+        return getReadyLineColor();
+
+      case UNACKED:
+        return getUnackedLineColor();
+
+      case TOTAL:
+        return getTotalLineColor();
+
+      default:
+        return defaultColor;
+    }
+  }
+
+  private void toggleSetVisibility(SetIndex index) {
+    LineData data = messagesChart.getData();
+    ILineDataSet set = data.getDataSetByIndex(index.getIndex());
+
+    if (set == null) {
+      return;
+    }
+
+    set.setVisible(!set.isVisible());
+  }
+
+  public enum SetIndex {
+
+    READY(0), UNACKED(1), TOTAL(2);
+
+    private int index;
+
+    SetIndex(int index) {
+      this.index = index;
+    }
+
+    public int getIndex() {
+      return index;
+    }
+  }
+
+  public enum VisibleRange {
+
+    ONE_MINUTE(TimeUnit.MINUTES.toSeconds(1)),
+
+    TEN_MINUTES(TimeUnit.MINUTES.toSeconds(10)),
+
+    ONE_HOUR(TimeUnit.HOURS.toSeconds(1)),
+
+    EIGHT_HOURS(TimeUnit.HOURS.toSeconds(8)),
+
+    ONE_DAY(TimeUnit.DAYS.toSeconds(1));
+
+    private long range;
+
+    VisibleRange(long range) {
+      this.range = range;
+    }
+
+    public static VisibleRange fromRange(int range) {
+      if (range == 0) {
+        return VisibleRange.ONE_MINUTE;
+      }
+
+      if (range == 1) {
+        return VisibleRange.TEN_MINUTES;
+      }
+
+      if (range == 2) {
+        return VisibleRange.ONE_HOUR;
+      }
+
+      if (range == 3) {
+        return VisibleRange.EIGHT_HOURS;
+      }
+
+      return VisibleRange.ONE_DAY;
+    }
+
+    public long getRange() {
+      return range;
+    }
+  }
+}
