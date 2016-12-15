d9ee84db794fdf0e69fefda421b8ae9066b105d0
Tomasz Dzieniak <tomasz.dzieniak@to-dev.com>
2016-12-15 14:04:32 +0100

    Add dependency that enables multidex shadowing in tests.

--- rabbitmq-management/src/test/java/com/todev/rabbitmqmanagement/ui/login/addservice/AddServicePresenterTest.java
+++ rabbitmq-management/src/test/java/com/todev/rabbitmqmanagement/ui/login/addservice/AddServicePresenterTest.java
@@ -32,9 +32,12 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
 public class AddServicePresenterTest extends BaseTest {
-  @Mock AddServiceContract.View view;
-  @Mock Consumer<Integer> onSuccess;
-  @Mock Service service;
+  @Mock
+  AddServiceContract.View view;
+  @Mock
+  Consumer<Integer> onSuccess;
+  @Mock
+  Service service;
 
   private AddServicePresenter presenter;
 
