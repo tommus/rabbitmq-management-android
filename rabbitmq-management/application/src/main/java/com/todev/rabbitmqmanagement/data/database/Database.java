d592c9eeca9b76226a55f3383dc70889c7a4b285
Tomasz Dzieniak <tomasz.dzieniak@windly.co>
2018-11-04 02:42:19 +0100

    Maintenance code cleanup - part 3.

--- rabbitmq-management/application/src/main/java/com/todev/rabbitmqmanagement/data/database/Database.java
+++
@@ -1,8 +0,0 @@
-package com.todev.rabbitmqmanagement.data.database;
-
-@com.raizlabs.android.dbflow.annotation.Database(name = Database.NAME,
-  version = Database.VERSION)
-public class Database {
-  static final String NAME = "RabbitMqManagementDatabase";
-  static final int VERSION = 1;
-}
--- rabbitmq-management/domain/src/main/java/co/windly/rabbitmqmanagement/domain/DomainComponent.java
+++ rabbitmq-management/domain/src/main/java/co/windly/rabbitmqmanagement/domain/DomainComponent.java
@@ -1,12 +1,10 @@
 package co.windly.rabbitmqmanagement.domain;
 
-import co.windly.rabbitmqmanagement.network.NetworkModule;
-import co.windly.rabbitmqmanagement.persistence.PersistenceModule;
 import dagger.Component;
 import javax.inject.Singleton;
 
 @Singleton
-@Component(modules = { DomainModule.class, NetworkModule.class, PersistenceModule.class })
+@Component(modules = { DomainModule.class /*, NetworkModule.class, PersistenceModule.class */ })
 public interface DomainComponent {
 
   //region Injections
